775371ac8c3be40dda7abb004573ccf0c40094e4
Sareena Gill <87876959+sareenagill@users.noreply.github.com>
2022-01-17 19:31:22 -0500

    displayTemperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function displayTemperature(reponse) {
+  console.log(respone.data);
+}
 
+let apiKey = "c54ef4831aa33703bc0280271dfa5d24";
+
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
