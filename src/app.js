c92f87e678f5a96e3b266b9cfdd3d1d346ed8dcc
Sareena Gill <87876959+sareenagill@users.noreply.github.com>
2022-01-18 17:52:27 -0500

    Showing the current temp

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-function displayTemperature(reponse) {
-  console.log(respone.data);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "c54ef4831aa33703bc0280271dfa5d24";
-
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
